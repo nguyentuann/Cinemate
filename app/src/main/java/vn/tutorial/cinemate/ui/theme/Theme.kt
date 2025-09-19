ccf1bfcfbdf1bfbbd332ed1cb92df098e3edc016
Nhat Tuan <nhattuan242@gmail.com>
2025-09-19 23:28:00 +0700

    refactor: Update color scheme and remove HomeScreen
    This commit introduces a new color scheme for both light and dark themes and removes the HomeScreen from MainActivity.
    
    - Updated `Color.kt` with new primary, neutral, and system colors.
    - Redefined `lightScheme` and `darkScheme` in `Theme.kt` using the new colors.
    - Removed dynamic color theming based on Android S and above.
    - Removed the `HomeScreen` composable and its related logic from `MainActivity`.
    - Added "login" string resource in English and Vietnamese.

--- app/src/main/java/vn/tutorial/cinemate/ui/theme/Theme.kt
+++ app/src/main/java/vn/tutorial/cinemate/ui/theme/Theme.kt
@@ -22,80 +22,37 @@ data class ExtendedColorScheme(
     val customColor1: ColorFamily,
 )
 
-private val lightScheme = lightColorScheme(
-    primary = primaryLight,
-    onPrimary = onPrimaryLight,
-    primaryContainer = primaryContainerLight,
-    onPrimaryContainer = onPrimaryContainerLight,
-    secondary = secondaryLight,
-    onSecondary = onSecondaryLight,
-    secondaryContainer = secondaryContainerLight,
-    onSecondaryContainer = onSecondaryContainerLight,
-    tertiary = tertiaryLight,
-    onTertiary = onTertiaryLight,
-    tertiaryContainer = tertiaryContainerLight,
-    onTertiaryContainer = onTertiaryContainerLight,
-    error = errorLight,
-    onError = onErrorLight,
-    errorContainer = errorContainerLight,
-    onErrorContainer = onErrorContainerLight,
-    background = backgroundLight,
-    onBackground = onBackgroundLight,
-    surface = surfaceLight,
-    onSurface = onSurfaceLight,
-    surfaceVariant = surfaceVariantLight,
-    onSurfaceVariant = onSurfaceVariantLight,
-    outline = outlineLight,
-    outlineVariant = outlineVariantLight,
-    scrim = scrimLight,
-    inverseSurface = inverseSurfaceLight,
-    inverseOnSurface = inverseOnSurfaceLight,
-    inversePrimary = inversePrimaryLight,
-    surfaceDim = surfaceDimLight,
-    surfaceBright = surfaceBrightLight,
-    surfaceContainerLowest = surfaceContainerLowestLight,
-    surfaceContainerLow = surfaceContainerLowLight,
-    surfaceContainer = surfaceContainerLight,
-    surfaceContainerHigh = surfaceContainerHighLight,
-    surfaceContainerHighest = surfaceContainerHighestLight,
+val lightScheme = lightColorScheme(
+    primary = Red,
+    onPrimary = White,
+    secondary = SystemBlue,
+    onSecondary = White,
+
+    background = White,
+    onBackground = Black,
+
+    surface = GreyLight3,
+    onSurface = Black,
+
+    error = SystemRed,
+    onError = White
 )
 
-private val darkScheme = darkColorScheme(
-    primary = primaryDark,
-    onPrimary = onPrimaryDark,
-    primaryContainer = primaryContainerDark,
-    onPrimaryContainer = onPrimaryContainerDark,
-    secondary = secondaryDark,
-    onSecondary = onSecondaryDark,
-    secondaryContainer = secondaryContainerDark,
-    onSecondaryContainer = onSecondaryContainerDark,
-    tertiary = tertiaryDark,
-    onTertiary = onTertiaryDark,
-    tertiaryContainer = tertiaryContainerDark,
-    onTertiaryContainer = onTertiaryContainerDark,
-    error = errorDark,
-    onError = onErrorDark,
-    errorContainer = errorContainerDark,
-    onErrorContainer = onErrorContainerDark,
-    background = backgroundDark,
-    onBackground = onBackgroundDark,
-    surface = surfaceDark,
-    onSurface = onSurfaceDark,
-    surfaceVariant = surfaceVariantDark,
-    onSurfaceVariant = onSurfaceVariantDark,
-    outline = outlineDark,
-    outlineVariant = outlineVariantDark,
-    scrim = scrimDark,
-    inverseSurface = inverseSurfaceDark,
-    inverseOnSurface = inverseOnSurfaceDark,
-    inversePrimary = inversePrimaryDark,
-    surfaceDim = surfaceDimDark,
-    surfaceBright = surfaceBrightDark,
-    surfaceContainerLowest = surfaceContainerLowestDark,
-    surfaceContainerLow = surfaceContainerLowDark,
-    surfaceContainer = surfaceContainerDark,
-    surfaceContainerHigh = surfaceContainerHighDark,
-    surfaceContainerHighest = surfaceContainerHighestDark,
+val darkScheme = darkColorScheme(
+    primary = Red,
+    onPrimary = White,
+
+    secondary = SystemBlue,
+    onSecondary = Black,
+
+    background = Black,
+    onBackground = White,
+
+    surface = GreyDark2,
+    onSurface = White,
+
+    error = SystemRed,
+    onError = White
 )
 
 @Immutable
@@ -113,15 +70,7 @@ fun CinemateTheme(
     dynamicColor: Boolean = true,
     content: @Composable() () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> darkScheme
-        else -> lightScheme
-    }
+    val colorScheme = if (darkTheme) darkScheme else lightScheme
 
     val view = LocalView.current
     if (!view.isInEditMode) {
