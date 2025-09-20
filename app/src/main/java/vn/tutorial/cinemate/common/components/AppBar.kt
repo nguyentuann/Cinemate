4d84d5440fdf14730bb61a97d8f99bfaf4e2aed7
Nhat Tuan <nhattuan242@gmail.com>
2025-09-20 14:46:54 +0700

    feat: Add AppBar to SignInScreen and include Cinemate logo
    This commit introduces an `AppBar` component to the `SignInScreen` and adds the Cinemate logo as a drawable resource.
    
    - Created `AppBar.kt` with a composable function that displays a `TopAppBar` containing the Cinemate logo and a back navigation icon.
    - Added `logo.xml`, a vector drawable for the Cinemate logo.
    - Integrated the `AppBar` into the `SignInScreen` by wrapping its content within a `Scaffold` and placing the `AppBar` in the `topBar` slot.
    - Adjusted padding in `MainActivity` for the `SignInScreen`.

---
+++ app/src/main/java/vn/tutorial/cinemate/common/components/AppBar.kt
@@ -0,0 +1,48 @@
+package vn.tutorial.cinemate.common.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import vn.tutorial.cinemate.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    onBack: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            titleContentColor = MaterialTheme.colorScheme.onBackground,
+            actionIconContentColor = MaterialTheme.colorScheme.onBackground
+        )
+    )
+}
